0164f43315bec61633b77435123ac1f64d9fe2ea
acer <1692230407@qq.com>
2020-06-30 21:00:30 +0800

    调试接口

---
+++ interfaces/data_console.py
@@ -0,0 +1,5 @@
+import json
+
+class data_dict_interfaces():
+    def data_dict_put(self,data):
+        pass
--- interfaces/views.py
+++ interfaces/views.py
@@ -9,6 +9,7 @@ from django.views import View
 from django.shortcuts import render
 from interfaces.models import Interfaces
 from django.db import connections
+from django.db.models import Q
 
 class InterfacesPage(View):
     """
@@ -19,6 +20,7 @@ class InterfacesPage(View):
     4、实例方法的第二个参数为HttpRequest对象
     5、一定要返回HttpResponse对象或者HttpResponse子类对象
     """
+
     def get(self, request, pk = None):
         # 获取所有查询对象
         try:
@@ -26,11 +28,28 @@ class InterfacesPage(View):
                 res = Interfaces.objects.filter(id=pk)
             else:
                 res = Interfaces.objects.all()
-            return JsonResponse("<h2>GET请求：查询成功{}</h2>".format(res))
+            res = list(res)
+            print(res)
+            return HttpResponse("{}".format(res))
         except Exception as e:
-            return JsonResponse("<h2>GET请求：该查询对象不存在</h2>")
+            return HttpResponse("<h2>GET请求：该查询对象不存在</h2>")
 
-
+        # 关联查询
+        # 通过从表信息获取父表信息
+        # 从表模型类小名__从表字段名__查询表达式
+        # 查询集对象，只有使用的时候才进行查询操作（惰性查找）
+        # qs = Interfaces.objects.filter(interfaces__name__regex='^[0-9]')
+        # 逻辑关系查询
+        # 支持链式查询，可以使用多个filter方法进行过滤
+        # 同一行查询中的多个条件是与的关系
+        # qs =Interfaces.objects.filter(name__startswith="人").filter(programmer__contains="4")
+        # qs =Interfaces.objects.filter(name__startswith="人",programmer__contains="4")
+        # 同一行查询中的多个条件是或的关系
+        # Q(条件)|Q(条件)...
+        # qs = Interfaces.objects.filter(Q(leader__contains) | Q(programmer_contains = "4"))
+        # 特殊操作
+        # 根据name属性进行排序，不带“-”升序，带“-”降序
+        # Interfaces.objects.all().order_by('-name')
 
 
     def post(self, request, pk = None):
@@ -46,7 +65,7 @@ class InterfacesPage(View):
         try:
             if pk:
                 Interfaces.objects.filter(id = pk).update(name = "修改后id2的name值")
-                return JsonResponse("<h2>PUT请求：更新成功{}</h2>".format(Projects.objects.get(id=pk)), content_type='application/json', status=200)
+                return JsonResponse("<h2>PUT请求：更新成功{}</h2>".format(Interfaces.objects.get(id=pk)), content_type='application/json', status=200)
         except Exception as e:
             return JsonResponse("<h2>PUT请求：更新失败，不存在该对象</h2>")
 
