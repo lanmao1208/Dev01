e36471223573725e7cd14ab131d2129222e19fdb
acer <1692230407@qq.com>
2020-07-08 15:11:55 +0800

    添加课堂注释

--- interfaces/serializers.py
+++ interfaces/serializers.py
@@ -1,11 +1,33 @@
 from rest_framework import serializers
 # 一定要继承父类 序列化器类
 class InterfaceSerializer(serializers.Serializer):
+    """
+    可以定义序列化器类，来实现序列化和反序列化操作
+    a.一定要继承serializers.Serializer或者Serializer的子类
+    b.默认情况下，可以定义序列化器字段，序列化器字段名要与模型类中字段名相同
+    c.默认情况下，定义几个序列化器字段，那么就会返回几个数据（到前端，序列化输出的过程），前端也必须得传递这几个字段（反序列化过程）
+    """
     # 定义序列化输出条件
+    # d.CharField、BooleanField、IntegerField与模型类中的字段类型一一对应
+    # e.required参数默认为None，指定前端必须得传此字段，如果设置为False的话，前端可以不传此字段
+    # f.label和help_text -> verbose_name和help_text一一对应
+    # g.allow_null指定前端传递参数时可以传空值
+    # CharField字段，max_length指定该字段不能操作的字节参数，
     name = serializers.CharField(max_length=200,label='接口名称',help_text='接口名称')
+    # leader = serializers.CharField(max_length=200, label='项目负责人', help_text='项目负责人')
+    # h.如果某个字段指定read_only=True，那么此字段，前端在创建数据时（反序列化过程）可以不用传，但是一定会输出（序列化过程）
+    # i.字段不能同时指定read_only=True, required=True
+    # leader = serializers.CharField(max_length=200, label='项目负责人', help_text='项目负责人', read_only=True, required=True)
+    # 只写入不输出writer_only = True ,只输出不写入 read_only = True
     projects_id = serializers.IntegerField(label='所属项目ID',help_text='所属项目ID')
+    # tester = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员')
+    # j.如果某个字段指定write_only=True，那么此字段只能进行反序列化输入，而不会输出（创建数据时必须得传，但是不返回）
+    # k.可以给字段添加error_messages参数，为字典类型，字典的key为校验的参数名，值为校验失败之后错误提示
     tester = serializers.CharField(max_length=50, label='测试人员', help_text='测试人员')
-    desc = serializers.CharField(max_length=200, label='简要描述', help_text='简要描述')
+    # k.一个字段不同同时指定write_only=True, read_only=True
+    # tester = serializers.CharField(max_length=200, label='测试人员', help_text='测试人员', write_only=True, read_only=True)
+    desc = serializers.CharField(max_length=200, label='简要描述', help_text='简要描述',write_only=True,
+                                   error_messages={"required": "该字段必传1", "max_length": "长度不能操作200个字节"})
     # # 输出时间
     # creat_time = serializers.DateTimeField(format='%Y-%M-%D %H:%M:%S',required=True,read_only=True)
     # updata_time = serializers.DateTimeField(format='%Y-%M-%D %H:%M:%S',required=True,read_only=True)
--- interfaces/views.py
+++ interfaces/views.py
@@ -17,6 +17,16 @@ class InterfacesPage(View):
     """
 
     def get(self, request, pk = None):
+        """
+        1.可以使用序列化器类来进行序列化输出
+        a.instance参数可以传模型类对象
+        b.instance参数可以传查询集（多条记录），many=True
+        c.可以ProjectsSerializer序列化器对象，调用data属性，可以将模型类对象转化为Python中的数据类型
+        d.如果未传递many=True参数，那么序列化器对象.data，返回字典，否则返回一个嵌套字典的列表
+        :param request:
+        :param pk:
+        :return:
+        """
         # 获取查询对象
         result = {"data":{}}
         try:
@@ -30,11 +40,11 @@ class InterfacesPage(View):
             result["data"] = one_obj.data
             result["msg"] = "查询成功"
             result["code"] = 0
-            return JsonResponse(result,safe=False)
+            return JsonResponse(result,safe=False,status=201)
         except Exception as e:
             result["msg"] = "查询失败"
             result["code"] = 1
-            return JsonResponse(result)
+            return JsonResponse(result,status=400)
 
         # 关联查询
         # 通过从表信息获取父表信息
@@ -60,12 +70,18 @@ class InterfacesPage(View):
         result = {}
         # 判断传入参数是否为json或者字典格式
         try:
+            # a.获取新的项目信息并转化为python中数据类型（字典或者嵌套字典的列表）
             create_json_data = json.loads(create_data)
         except Exception as e:
             result["msg"] = "参数错误"
             result["code"] = 1
             return JsonResponse(result,status=400)
-
+        # 1、创建序列化器对象
+        # a.把前端传递的json格式参数转化为字典之后，传递给data参数
+        # b.序列化器对象.is_valid()方法，开始进行校验，如果不调用此方法，那么不会进行校验
+        # c.调用序列化器对象.is_valid()方法，如果校验成功，返回True，否则返回False
+        # d.必须调用is_valid()方法之后，才能使用.errors属性去获取报错信息，相当于一个字典
+        # e.必须调用is_valid()方法之后，才能使用.validated_data属性去获取校验通过信息，相当于一个字典
         rsf = InterfaceSerializer(data=create_json_data)
         # 效验数据是否符合接口要求的参数设置
         try:
@@ -76,11 +92,12 @@ class InterfacesPage(View):
             result.update(rsf.errors)
             return JsonResponse(result,status=400)
         # 效验通过后进行创建
+        # c.创建项目
         Interfaces.objects.create(**rsf.validated_data)
         result.update(rsf.validated_data)
         result["msg"] = "创建成功"
         result["code"] = 0
-        return JsonResponse(result, safe=False,status=200)
+        return JsonResponse(result, safe=False,status=201)
 
     def put(self, request, pk):
         # 更新id为pk，更新内容通过json传递
@@ -113,7 +130,7 @@ class InterfacesPage(View):
         res.save()
         result["msg"] = "更新成功"
         result["code"] = 0
-        return JsonResponse(result,status=200)
+        return JsonResponse(result,status=201)
 
     def delete(self, request, pk):
         # pk为指定删除对象的id
@@ -122,8 +139,8 @@ class InterfacesPage(View):
             Interfaces.objects.filter(id = pk).delete()
             result["msg"] = "删除成功"
             result["code"] = 0
-            return JsonResponse(result)
+            return JsonResponse(result,status=201)
         except Exception as e:
-            result["msg"] = "删除失败"
+            result["msg"] = "删除失败，查询不到指定id"
             result["code"] = 1
-            return JsonResponse(result)
+            return JsonResponse(result,status=400)
